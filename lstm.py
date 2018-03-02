d0de0b2d4a31f6be0da31fd14c6a166ac0914f48
hermafr <hertelm@informatik.uni-freiburg.de>
2018-03-02 13:34:18 +0100

    set up random search for task 3

--- lstm.py
+++ lstm.py
@@ -4,6 +4,13 @@ from sklearn.model_selection import KFold
 from util.loader import load_data
 from util.time_series_data import get_time_series
 from models.lstm import lstm
+from preprocessing.standard_scaler import StandardScaler
+
+lr = 0.1
+scale_configs = True
+regularize = False
+alpha = 0.001
+repeat_config = False
 
 def predict(model, X):
     """
@@ -21,7 +28,7 @@ def evaluate(model, X, y):
     mse = np.mean(np.power(np.array(y) - np.array(y_hat), 2))
     return mse
 
-def main(n_steps=5, epochs=100, repeat_config=False):
+def main(n_steps=10, epochs=200):
     """
     Trains and evaluates the MSE of a LSTM model in a 3-fold CV.
     
@@ -31,30 +38,40 @@ def main(n_steps=5, epochs=100, repeat_config=False):
         time step, otherwise only in the first time step
     """
     configs, learning_curves = load_data(source_dir='./data')
-    X = get_time_series(configs, learning_curves, repeat_config=repeat_config)
     Y = [curve[-1] for curve in learning_curves]
     
     n_params = len(configs[0])
     d = n_params + 1
-    
-    X = [x[:n_steps,:] for x in X]
     
     # 3 fold CV:
     n_folds = 3
     k_fold = KFold(n_splits=n_folds)
     fold_mses = []
     fold = 0
-    for training_indices, test_indices in k_fold.split(X):
+    for training_indices, test_indices in k_fold.split(Y):
         fold = fold + 1
         print("***** FOLD %i *****" % fold)
         
-        X_train = [X[index] for index in training_indices]
+        # prepare training data:
+        configs_train = [configs[index] for index in training_indices]
+        learning_curves_train = [learning_curves[index][:n_steps] for index in training_indices]
+        if scale_configs:
+            scaler = StandardScaler()
+            configs_train = scaler.fit_transform(configs_train)
+        X_train = get_time_series(configs_train, learning_curves_train, repeat_config=repeat_config)
         Y_train = [Y[index] for index in training_indices]
-        X_test = [X[index] for index in test_indices]
+        
+        # prepare test data:
+        configs_test = [configs[index] for index in test_indices]
+        learning_curves_test = [learning_curves[index][:n_steps] for index in test_indices]
+        if scale_configs:
+            configs_test = scaler.transform(configs_test)
+        X_test = get_time_series(configs_test, learning_curves_test, repeat_config=repeat_config)
         Y_test = [Y[index] for index in test_indices]
+
         n_train = len(training_indices)
         
-        model = lstm(d)
+        model = lstm(d, lr, regularize = regularize, alpha = alpha)
         
         # training:
         for epoch in range(epochs):
@@ -70,11 +87,11 @@ def main(n_steps=5, epochs=100, repeat_config=False):
             mse_test = evaluate(model, X_test, Y_test)
             print("training mse = %f" % mse_train)
             print("test mse = %f" % mse_test)
-            if (epoch + 1) % 10 == 0:
-                predictions = predict(model, X_test)
-                print("y_hat", "y")
-                for y_hat, y in zip(predictions, Y_test):
-                    print(y_hat, y)
+            #if (epoch + 1) % 10 == 0:
+            #    predictions = predict(model, X_test)
+            #    print("y_hat", "y")
+            #    for y_hat, y in zip(predictions, Y_test):
+            #        print(y_hat, y)
         
         # evaluation:
         fold_mses.append(evaluate(model, X_test, Y_test))
