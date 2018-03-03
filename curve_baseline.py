115197e6360b26bd6f817e1b3142fdcf7bf09362
Aleksey Zhelo <alekseyzhelo@yandex.ru>
2018-03-03 15:39:11 +0100

    Task 3 point 2 baseline, improved task 3 last point baseline, new results, moved March meeting file to reports

--- curve_baseline.py
+++ curve_baseline.py
@@ -2,15 +2,20 @@ from __future__ import print_function
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+# noinspection PyUnresolvedReferences
 from sklearn.ensemble import GradientBoostingRegressor, ExtraTreesRegressor, AdaBoostRegressor, RandomForestRegressor, \
     BaggingRegressor
-from sklearn.model_selection import KFold
+# noinspection PyUnresolvedReferences
 from sklearn.svm import SVR
+# noinspection PyUnresolvedReferences
+from sklearn import linear_model
+
+from sklearn.model_selection import KFold
 
 from util.common import loss
 from util.loader import load_data_as_numpy
 from sklearn.preprocessing import StandardScaler
-from sklearn import linear_model
 
 
 def predict_curve(model, config, start_points, prediction_length):
@@ -35,59 +40,70 @@ def predict_curve(model, config, start_points, prediction_length):
 normalize = True
 configs, learning_curves = load_data_as_numpy()
 
-best_model = None
+estimators = [
+    'GradientBoostingRegressor(learning_rate=0.033, n_estimators=300)',
+    'linear_model.SGDRegressor(loss=\'squared_loss\')',
+    'linear_model.SGDRegressor(loss=\'epsilon_insensitive\', epsilon=0.005)',
+    'linear_model.LinearRegression()',
+    'linear_model.Ridge(alpha=0.1)',
+    'RandomForestRegressor(n_estimators=30)',
+    'SVR(C=2.0, kernel=\'linear\', epsilon=0.005)',
+    'BaggingRegressor()'
+]
 
 k_fold = KFold(n_splits=3, shuffle=True, random_state=1)
-performances = np.zeros((3, 4))
 
-current_fold = 0
-for train_indices, test_indices in k_fold.split(configs):
-    # split into training and test data
-    train_configs = configs[train_indices]
-    train_curves = learning_curves[train_indices]
-    test_configs = configs[test_indices]
-    test_curves = learning_curves[test_indices]
+for m_idx, model_desc in enumerate(estimators):
+    performances = np.zeros((3, 4))
+    current_fold = 0
 
-    if normalize:
-        scaler = StandardScaler()
-        train_configs = scaler.fit_transform(train_configs)
-        test_configs = scaler.transform(test_configs)
+    print(model_desc)
+    for train_indices, test_indices in k_fold.split(configs):
+        # split into training and test data
+        train_configs = configs[train_indices]
+        train_curves = learning_curves[train_indices]
+        test_configs = configs[test_indices]
+        test_curves = learning_curves[test_indices]
 
-    train_x = np.zeros((train_configs.shape[0] * 36, 9))
-    train_y = np.zeros(train_configs.shape[0] * 36)
+        if normalize:
+            scaler = StandardScaler()
+            train_configs = scaler.fit_transform(train_configs)
+            test_configs = scaler.transform(test_configs)
 
-    for i in range(train_configs.shape[0]):
-        for j in range(4, train_curves.shape[1]):
-            train_x[i * 36 + j - 4, :5] = train_configs[i]
-            train_x[i * 36 + j - 4, 5:9] = train_curves[i, j - 4:j]
-            train_y[i * 36 + j - 4] = train_curves[i, j]
+        train_x = np.zeros((train_configs.shape[0] * 36, 9))
+        train_y = np.zeros(train_configs.shape[0] * 36)
 
-    # clf = SVR(C=2.0, kernel='linear', epsilon=0.005)
-    # clf = RandomForestRegressor(n_estimators=30)
-    clf = GradientBoostingRegressor(learning_rate=0.033, n_estimators=300)
-    # clf = linear_model.SGDRegressor(loss='squared_loss')
-    # clf = linear_model.SGDRegressor(loss='epsilon_insensitive')  # seems to be much worse
-    clf.fit(train_x, train_y)
+        for i in range(train_configs.shape[0]):
+            for j in range(4, train_curves.shape[1]):
+                train_x[i * 36 + j - 4, :5] = train_configs[i]
+                train_x[i * 36 + j - 4, 5:9] = train_curves[i, j - 4:j]
+                train_y[i * 36 + j - 4] = train_curves[i, j]
 
-    for k, start_point in enumerate([4, 9, 19, 29]):
-        pred_curves = np.array([predict_curve(clf, test_configs[t], test_curves[t, :start_point], test_curves.shape[1])
-                                for t in range(test_configs.shape[0])])
-        fold_loss = loss(pred_curves[:, -1], test_curves[:, -1])
-        performances[current_fold, k] = fold_loss
-    print('fold {0} loss: {1}'.format(current_fold, performances[current_fold]))
-    current_fold += 1
+        clf = eval(model_desc)
+        clf.fit(train_x, train_y)
 
-print('mean CV performance: {0}'.format(performances.mean(axis=0)))
+        # for k, input_points in enumerate([4, 9, 19, 29]):
+        for k, input_points in enumerate([5, 10, 20, 30]):
+            pred_curves = np.array(
+                [predict_curve(clf, test_configs[t], test_curves[t, :input_points], test_curves.shape[1])
+                 for t in range(test_configs.shape[0])]
+            )
+            fold_loss = loss(pred_curves[:, -1], test_curves[:, -1])
+            performances[current_fold, k] = fold_loss
+        print('fold {0} loss: {1}'.format(current_fold, performances[current_fold]))
+        current_fold += 1
 
-steps = test_curves.shape[1]
-fig = plt.figure(figsize=(10, 10))
-for i, idx in enumerate(np.random.choice(np.arange(0, test_configs.shape[0]), 20)):
-    y_hat = predict_curve(clf, test_configs[idx], test_curves[idx, :4], steps)
+    print('mean CV performance: {0} \n'.format(performances.mean(axis=0)))
 
-    plt.subplot(4, 5, i + 1)
-    plt.plot(range(40), test_curves[idx], "g")
-    plt.plot(range(steps), y_hat, "r")
-    plt.ylim(0, 1)
-
-plt.tight_layout()
-plt.show()
+# steps = test_curves.shape[1]
+# fig = plt.figure(figsize=(10, 10))
+# for i, idx in enumerate(np.random.choice(np.arange(0, test_configs.shape[0]), 20)):
+#     y_hat = predict_curve(clf, test_configs[idx], test_curves[idx, :4], steps)
+#
+#     plt.subplot(4, 5, i + 1)
+#     plt.plot(range(40), test_curves[idx], "g")
+#     plt.plot(range(steps), y_hat, "r")
+#     plt.ylim(0, 1)
+#
+# plt.tight_layout()
+# plt.show()
---
+++ final_error_from_n_steps_baseline.py
@@ -0,0 +1,78 @@
+from __future__ import print_function
+
+import numpy as np
+
+# noinspection PyUnresolvedReferences
+from sklearn.ensemble import GradientBoostingRegressor, ExtraTreesRegressor, AdaBoostRegressor, RandomForestRegressor, \
+    BaggingRegressor
+# noinspection PyUnresolvedReferences
+from sklearn.svm import SVR
+# noinspection PyUnresolvedReferences
+from sklearn import linear_model
+
+from sklearn.model_selection import KFold
+
+from util.common import loss
+from util.loader import load_data_as_numpy
+from sklearn.preprocessing import StandardScaler
+
+normalize = True
+use_config = True
+configs, learning_curves = load_data_as_numpy()
+
+estimators = [
+    'GradientBoostingRegressor(learning_rate=0.033, n_estimators=300)',
+    'linear_model.SGDRegressor(loss=\'squared_loss\')',
+    'linear_model.SGDRegressor(loss=\'epsilon_insensitive\', epsilon=0.005)',
+    'linear_model.LinearRegression()',
+    'linear_model.Ridge(alpha=0.1)',
+    'RandomForestRegressor(n_estimators=30)',
+    'SVR(C=2.0, kernel=\'linear\', epsilon=0.005)',
+    'BaggingRegressor()'
+]
+
+k_fold = KFold(n_splits=3, shuffle=True, random_state=1)
+
+for m_idx, model_desc in enumerate(estimators):
+    performances = np.zeros((3, 4))
+    current_fold = 0
+
+    print(model_desc)
+    for train_indices, test_indices in k_fold.split(configs):
+        # split into training and test data
+        train_configs = configs[train_indices]
+        train_curves = learning_curves[train_indices]
+        test_configs = configs[test_indices]
+        test_curves = learning_curves[test_indices]
+
+        if normalize:
+            scaler = StandardScaler()
+            train_configs = scaler.fit_transform(train_configs)
+            test_configs = scaler.transform(test_configs)
+
+        for k, input_points in enumerate([5, 10, 20, 30]):
+            if use_config:
+                train_x = np.zeros((train_configs.shape[0], 5 + input_points))
+                test_x = np.zeros((test_configs.shape[0], 5 + input_points))
+
+                train_x[:, :5] = train_configs[:]
+                train_x[:, 5:5 + input_points] = train_curves[:, :input_points]
+
+                test_x[:, :5] = test_configs[:]
+                test_x[:, 5:5 + input_points] = test_curves[:, :input_points]
+            else:
+                train_x = train_curves[:, :input_points]
+                test_x = test_curves[:, :input_points]
+
+            train_y = train_curves[:, -1]
+
+            clf = eval(model_desc)
+            clf.fit(train_x, train_y)
+
+            pred_y = clf.predict(test_x)
+            fold_loss = loss(pred_y, test_curves[:, -1])
+            performances[current_fold, k] = fold_loss
+        print('fold {0} loss: {1:}'.format(current_fold, np.round(performances[current_fold], 6)))
+        current_fold += 1
+
+    print('mean CV performance: {0} \n'.format(np.round(performances.mean(axis=0), 6)))
